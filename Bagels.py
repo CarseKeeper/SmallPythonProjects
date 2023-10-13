924f73b866a66810b33e2aa4ec999a3433ed9e26
CarseKeeper <125390842+CarseKeeper@users.noreply.github.com>
2023-10-13 11:48:15 -0500

    Update Bagels.py
    Made print statements more verbose

--- Bagels.py
+++ Bagels.py
@@ -1,26 +1,32 @@
 import random
+import os
 
 NUM_DIGITS:int = 3
 MAX_GUESSES:int = 10
 
 def main():
-    print('''Welcome to Bagels!\nI am thinking of a {}-digit code without repeated digits\nHere are some hints to help with your Guessing:
+    try:
+        os.system('cls')
+    except(Exception):
+        os.system('clear')
+    
+    print('''Welcome to Bagels!\nI am thinking of a {}-digit code without repeated digits\n\nHere are some hints to help with your Guessing:
     When I say:       That means:
         Pico                One digit is correct but in the wrong position
         Fermi               One digit is correct and in the correct position
         Bagels              No digit is correct
-        E.X.
-        If the number was 248 and you guessed 498, the clue would be Fermi Pico'''.format(NUM_DIGITS))
+E.X.
+If the number was 248 and you guessed 498, the clue would be Fermi Pico'''.format(NUM_DIGITS))
     
     while True:     #Game loop
         secretNumber = getSecretNumber()
-        print('I have thought of a number.\nYou have {} guesses to get it.'.format(MAX_GUESSES))
+        print('\nI have thought of a number.\nYou have {} guesses to get it.'.format(MAX_GUESSES))
         
         numGuesses:int = 1
         while numGuesses <= MAX_GUESSES:
             guess:str = ''
             while len(guess) != NUM_DIGITS or not guess.isdecimal():
-                print('Guess #{}: '.format(numGuesses))
+                print('\nGuess #{}: '.format(numGuesses))
                 guess = input('> ')
                 
             clues:str = getClues(guess, secretNumber)
@@ -33,7 +39,7 @@ def main():
                 print('You ran out of guesses.')
                 print('The answer was {}.'.format(secretNumber))
                 
-        print('Do you want to play again? (yes/no)')
+        print('\nDo you want to play again? (yes/no)')
         if not input('> ').lower().startswith('y'):
             break
     print('Thanks for playing!')
