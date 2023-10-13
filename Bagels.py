3c6e411770d9e2e7ffedf3e1ab42bec67c0f33f7
CarseKeeper <125390842+CarseKeeper@users.noreply.github.com>
2023-10-13 11:04:47 -0500

    Finished Bagels
    Finished the getClues method and finished functionality of the game

--- Bagels.py
+++ Bagels.py
@@ -4,17 +4,16 @@ NUM_DIGITS:int = 3
 MAX_GUESSES:int = 10
 
 def main():
-    print('''Welcome to Bagels!\nI am thinking of a {}-number code without repeated digits
-          Here are some hints to help with your Guessing:
-          When I say:       That means:
-            Pico                One digit is correct but in the wrong position
-            Fermi               One digit is correct and in the correct position
-            Bagels              No digit is correct
-            E.X.
-            If the number was 248 and you guessed 498, the clue would be Fermi Pico'''.format(NUM_DIGITS))
+    print('''Welcome to Bagels!\nI am thinking of a {}-digit code without repeated digits\nHere are some hints to help with your Guessing:
+    When I say:       That means:
+        Pico                One digit is correct but in the wrong position
+        Fermi               One digit is correct and in the correct position
+        Bagels              No digit is correct
+        E.X.
+        If the number was 248 and you guessed 498, the clue would be Fermi Pico'''.format(NUM_DIGITS))
     
     while True:     #Game loop
-        secretNumber:str = getSecretNumber()
+        secretNumber = getSecretNumber()
         print('I have thought of a number.\nYou have {} guesses to get it.'.format(MAX_GUESSES))
         
         numGuesses:int = 1
@@ -39,11 +38,32 @@ def main():
             break
     print('Thanks for playing!')
     
-    def getSecretNumber() -> str:
-        #returns a string of ints of length NUM_DIGITS unique random digits.
-        numbers = list('0123456789')
-        random.shuffle(numbers)
-        secretNumber:str = ''
-        for i in range(NUM_DIGITS):
-            secretNumber += i
-        return secretNumber
+def getSecretNumber():
+    #returns a string of ints of length NUM_DIGITS unique random digits.
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+    secretNumber:str = ''
+    for i in range(NUM_DIGITS):
+        secretNumber += numbers[i]
+    return secretNumber
+    
+def getClues(guess, secretNumber):
+    if guess == secretNumber:
+        return 'You got it!'
+    
+    clues: list[str] = []
+    
+    for i in range(len(guess)):
+        if guess[i] == secretNumber[i]:
+            clues.append('Fermi')
+        elif guess[i] in secretNumber:
+            clues.append('Pico')    
+    if len(clues) == 0:
+        return 'Bagels'
+    else:
+        clues.sort()
+        return ''.join(clues)
+        
+        
+if __name__ == '__main__':
+    main()
