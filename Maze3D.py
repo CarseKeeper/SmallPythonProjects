0d8034a51a5a64867ae935418a2b533d19c02f87
CarseKeeper <125390842+CarseKeeper@users.noreply.github.com>
2023-10-15 18:39:32 -0500

    Completed basic Maze game
    Completed basic game and downloaded mazes from https://invpy.com/mazes/

--- Maze3D.py
+++ Maze3D.py
@@ -82,7 +82,7 @@ CLOSED['D'] = wallStrToWallDict(r'''
 
 CLOSED['E'] = wallStrToWallDict(r'''
 ..\..
-...\_
+...\.
 ....|
 ....|
 ....|
@@ -97,7 +97,7 @@ CLOSED['E'] = wallStrToWallDict(r'''
 
 CLOSED['F'] = wallStrToWallDict(r'''
 ../..
-_/...
+./...
 |....
 |....
 |....
@@ -123,4 +123,140 @@ def displayWallDict(wallDict):
     print(BLOCK * (wallDict['width'] + 2))
     
 def pasteWallDict(srcWallDict, distWallDict, left, top):
-    pass
+    distWallDict = copy.copy(distWallDict)
+    for x in range(srcWallDict['width']):
+        for y in range(srcWallDict['height']):
+            distWallDict[(x + left,y + top)] = srcWallDict[(x,y)]
+    return distWallDict
+
+def makeWallDict(maze, playerx, playery, playerDirection, exitx, exity):
+    if playerDirection == NORTH:
+        offsets = (('A', 0, -2), ('B', -1, -1), ('C', 0, -1), 
+                   ('D', 1, -1), ('E', -1, 0), ('F', 1, 0))
+    if playerDirection == SOUTH:
+        offsets = (('A', 0, 2), ('B', 1, 1), ('C', 0, 1), 
+                   ('D', -1, 1), ('E', 1, 0), ('F', -1, 0))
+    if playerDirection == EAST:
+        offsets = (('A', 2, 0), ('B', 1, -1), ('C', 1, 0),
+                   ('D', 1, 1), ('E', 0, -1), ('F', 0, 1))
+    if playerDirection == WEST:
+        offsets = (('A', -2, 0), ('B', -1, 1), ('C', -1, 0),
+                   ('D', -1, -1), ('E', 0, 1), ('F', 0, -1))
+    
+    section = {}
+    for sec, xOff, yOff in offsets:
+        section[sec] = maze.get((playerx + xOff, playery + yOff), WALL)
+        if (playerx + xOff, playery + yOff) == (exitx, exity):
+            section[sec] = EXIT
+    
+    wallDict = copy.copy(ALL_OPEN)
+    PASTE_CLOSED_TO = {'A': (6,4), 'B': (4,3), 'C': (3,1),
+                       'D': (10,3), 'E': (0,0), 'F': (12,0)}
+    
+    for sec in 'ABDCEF':
+        if section[sec] == WALL:
+            wallDict = pasteWallDict(CLOSED[sec], wallDict,
+                                     PASTE_CLOSED_TO[sec][0], PASTE_CLOSED_TO[sec][1])
+            
+    if section['C'] == EXIT:
+        wallDict = pasteWallDict(EXIT_DICT, wallDict, 7, 9)
+    if section['E'] == EXIT:
+        wallDict = pasteWallDict(EXIT_DICT, wallDict, 0, 11)
+    if section['F'] == EXIT:
+        wallDict = pasteWallDict(EXIT_DICT, wallDict, 13, 11)
+
+    return wallDict
+
+while True:
+    filename = input("Input filename, LIST for maze files, or QUIT\n>")
+    if filename.upper() == 'LIST':
+        print('MAZE FILES IN ', os.getcwd())
+        for file in os.listdir():
+            if(file.startswith('maze') and file.endswith('.txt')):
+                print('\t', file)
+        continue
+    
+    if filename.upper() == 'QUIT':
+        sys.exit()
+    
+    if os.path.exists(filename):
+        break
+    print('Couldn\'t find file named ', filename)
+    
+
+print('Maze Runner 3D!!!')
+
+mazeFile = open(filename)
+maze = {}
+lines = mazeFile.readlines()
+px = None
+py = None
+exitx = None
+exity = None
+y = 0
+for line in lines:
+    WIDTH = len(line.rstrip())
+    for x, character in enumerate(line.rstrip()):
+        assert character in (WALL, EMPTY, START, EXIT), 'Invalid character at column {1}, line {2}'.format(x+1, y+1)
+        if character in (WALL, EMPTY):
+            maze[(x,y)] = character
+        elif character == START:
+            px, py = x, y
+            maze[(x,y)] = EMPTY
+        elif character == EXIT:
+            exitx, exity = x, y
+            maze[(x,y)] = EMPTY
+    y += 1
+HEIGHT = y
+    
+assert px != None and py != None, 'No START point in maze. :('
+assert exitx != None and exity != None, 'No EXIT point in maze. :('
+pDir = NORTH
+
+while True:
+    displayWallDict(makeWallDict(maze, px, py, pDir, exitx, exity))
+    
+    while True:
+        print('Location ({},{}) Direction: {}'.format(px,py,pDir))
+        print('                    (W)')
+        print('Enter direction: (A)   (D) or QUIT.')
+        move = input('> ').upper()
+        
+        if move == 'QUIT':
+            print('Thanks for playing.')
+            sys.exit()
+        
+        if (move not in ['F', 'L', 'R', 'W', 'A', 'D'] and not move.startswith('T')):
+            print('Please enter one of F, L, or R (or W, A, D).')
+            continue
+        
+        if move == 'F' or move == 'W':
+            if pDir == NORTH and maze[(px,py - 1)] == EMPTY:
+                py -= 1
+                break
+            if pDir == SOUTH and maze[(px,py + 1)] == EMPTY:
+                py += 1
+                break
+            if pDir == EAST and maze[(px + 1,py)] == EMPTY:
+                px += 1
+                break
+            if pDir == WEST and maze[(px - 1,py)] == EMPTY:
+                px -= 1
+                break
+        elif move == 'L' or move == 'A':
+            pDir = {NORTH: WEST, WEST: SOUTH, SOUTH: EAST, EAST: NORTH}[pDir]
+            break
+        elif move == 'R' or move == 'D':
+            pDir = {NORTH: EAST, EAST: SOUTH, SOUTH: WEST, WEST: NORTH}[pDir]
+            break
+        elif move.startswith('T'):
+            px, py = move.split()[1].split(',')
+            px = int(px)
+            py = int(py)
+            break
+        else:
+            print('You can\'t move there.')
+            
+    if (px,py) == (exitx,exity):
+        print('You solved the maze! :)\n Thanks for Playing!')
+        sys.exit()
